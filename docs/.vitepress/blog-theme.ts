f32d46182cbd4d987380f29ddf8b9974f588f3a1
RG <13452334233@163.com>
2025-04-10 23:28:02 +0800

    fix: 修改图标和部分配置

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -29,13 +29,14 @@ const blogTheme = getThemeConfig({
 
   // 页脚
   footer: {
+    version: false,
     // message 字段支持配置为HTML内容，配置多条可以配置为数组
     // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
-    copyright: 'MIT License | 粥里有勺糖',
-    // icpRecord: {
-    //   name: '蜀ICP备19011724号',
-    //   link: 'https://beian.miit.gov.cn/'
-    // },
+    // copyright: 'MIT License | 粥里有勺糖',
+    icpRecord: {
+      name: '蜀ICP备2022029293号-1',
+      link: 'https://beian.miit.gov.cn/'
+    },
     // securityRecord: {
     //   name: '公网安备xxxxx',
     //   link: 'https://www.beian.gov.cn/portal/index.do'
